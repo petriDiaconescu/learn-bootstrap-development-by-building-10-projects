51edd2ef60ccfe18bca47bbb61295d852f781eec
petrero <petrero@gmail.com>
2017-12-13 20:22:05 +0200

    Configure gulp to watch changes in sass files from scss folder and transform them in css files. Report errors if something is wrong.Run in console: gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('sass', function(){
+  gulp.src('sass/*.scss')
+    .pipe(plugins.plumber(function(error){
+      console.log(error.toString());
+      this.emit('end');
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('sass/*.scss', ['sass'])
+});
+
+gulp.task('default', ['sass', 'watch']);
